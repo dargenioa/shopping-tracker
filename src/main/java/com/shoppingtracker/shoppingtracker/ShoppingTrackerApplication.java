c49915b5758cdd709ee3cd9c12fd3246e05560c8
Amanda D'Argenio <dargenioa14@gmail.com>
2023-02-24 15:46:40 -0500

    inital comment with setup of sql and flyway, need to add model for entity manager

---
+++ src/main/java/com/shoppingtracker/shoppingtracker/ShoppingTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.shoppingtracker.shoppingtracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingTrackerApplication.class, args);
+	}
+
+}
---
+++ src/main/resources/db/migration/V1_create_table_shopping_tracker.sql
@@ -0,0 +1,1 @@
+CREATE TABLE [IF NOT EXISTS] shopping_tracker
